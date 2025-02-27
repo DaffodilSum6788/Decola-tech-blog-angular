fafd393274d59d92a625bec5eede8609b67c6508
DaffodilSum6788 <lucasrobertopereirabal@gmail.com>
2025-02-27 20:02:01 -0300

    Adição de arquivos

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Por que trabalhar na avanade?",
+    "description": "A Avanade é uma das maiores empresas de tecnologia e serviços de consultoria do mundo, com escritórios no Brasil em Recife. Além de um histórico constante de inovação no mercado de desenvolvimento de software, a Avanade oferece um dos melhores ambientes de trabalho para profissionais de todos os perfis, e é uma ótima oportunidade para desenvolvedores construírem uma carreira duradoura.",
+    "photoCover":"https://vagas.byintera.com/wp-content/uploads/2021/05/Previa-Preview-Starbucks-Capa-da-Landing-Page.png"
+  },
+  {
+    "id":"2",
+    "title": "ANGULAR LANÇA NOVA VERSÃO, CONFIRA NOVIDADES",
+    "description": "O Angular, no final de 2024, lançou sua mais nova versão: O Angular v19. Essa nova versão do framework de desenvolvimento front-end traz como melhoria principal a hidratação incremental, permitindo que partes da aplicação tornem-se interativas conforme o usuário interage com elas.",
+    "photoCover":"https://miro.medium.com/v2/resize:fit:720/format:webp/1*IKZuN_kzUYPl37lBXQkymQ.png"
+  },
+  {
+    "id":"3",
+    "title": "Por que utilizar Spring Boot?",
+    "description": "O Spring Boot é uma ferramenta de desenvolvimento de aplicações com Spring, um framework da linguagem Java. Vem se tornando cada vez mais importante no desenvolvimento de aplicações Java, pois sua estrutura possui muitos recursos, como configurações automáticas e servidores embutidos, facilitando ao máximo a criação de projetos como APIs.",
+    "photoCover":"https://rdrblog.com.br/wp-content/uploads/2020/08/Spring-BOOT-Interview-questions-1.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Typescript: A linguagem que muda o jogo",
+    "description": "Typescript é uma linguagem de programação desenvolvida pela Microsoft, projetada como um superconjunto da linguagem Javascript. Ela introduz muitas melhorias que facilitam o desenvolvimento e o tornam mais seguro, como a tipagem de variáveis, sugestões inteligentes de código, compatibilidade completa com programas em Javascript e refatoração automatizada, que tornam o Typescript uma linguagem cada vez mais requisitada no mercado e cada vez mais respeitada pela comunidade mundial de desenvolvedores.",
+    "photoCover":"https://media.licdn.com/dms/image/v2/D4D12AQEmxmyTkrPGbw/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1690674031231?e=2147483647&v=beta&t=Xd4uSkVfRWWT1c0wMWFzysavRDoyJ8Nhcnyky_zhokk"
+  }
+]
